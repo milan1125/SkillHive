00488a1c428a558d8c4a8f0f12ee70527e30b08f
milan1125 <milansavaliya1125@gmail.com>
2025-07-01 01:35:48 +0530

    Resolve Error : multer

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,4 +1,25 @@
-import multer from "multer";
+// import multer from "multer";
 
-const upload = multer({dest:"uploads/"});
+// const upload = multer({dest:"uploads/"});
+// export default upload
+
+
+import multer from "multer";
+import path from "path";
+import os from "os";
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./Public/Temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+const storage = multer.diskStorage({
+  destination: os.tmpdir(),
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 export default upload
